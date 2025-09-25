a33866d093c0786f73d1084c835ca66ecbc38fe2
Vivek Patel <ce.vrp26@gmail.com>
2025-09-25 17:32:21 -0600

    Upodate lambda test message

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ exports.handler = async (event) => {
     const response = {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Hello World I am Vicky, how are you I am gameover',
+            message: 'Hello World I am Vicky,CICD pipeline testing',
             timestamp: new Date().toISOString(),
             event: event
         }),
